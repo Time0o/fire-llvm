21769b6202f9caf94ebbae2a6b6e175068973491
Timo Nicolai <timonicolai@arcor.de>
2021-10-20 22:02:57 +0200

    Rewrite file in memory

--- source/Fire.cpp
+++ source/Fire.cpp
@@ -4,18 +4,19 @@
 #include <string>
 #include <vector>
 
-#include "clang/Basic/Diagnostic.h"
-#include "clang/Basic/DiagnosticIDs.h"
-#include "clang/Basic/SourceLocation.h"
 #include "clang/AST/ASTConsumer.h"
 #include "clang/AST/ASTContext.h"
 #include "clang/AST/Decl.h"
 #include "clang/AST/Expr.h"
 #include "clang/ASTMatchers/ASTMatchFinder.h"
 #include "clang/ASTMatchers/ASTMatchers.h"
+#include "clang/Basic/Diagnostic.h"
+#include "clang/Basic/DiagnosticIDs.h"
+#include "clang/Basic/SourceLocation.h"
 #include "clang/Frontend/CompilerInstance.h"
 #include "clang/Frontend/FrontendAction.h"
 #include "clang/Frontend/FrontendPluginRegistry.h"
+#include "clang/Rewrite/Core/Rewriter.h"
 
 #include "llvm/ADT/StringRef.h"
 #include "llvm/Support/Casting.h"
@@ -44,6 +45,10 @@ private:
 class FireMatchCallback : public clang::ast_matchers::MatchFinder::MatchCallback
 {
 public:
+  FireMatchCallback(clang::Rewriter *Rewriter)
+  : Rewriter_(Rewriter)
+  {}
+
   void run(clang::ast_matchers::MatchFinder::MatchResult const &Result) override
   {
     auto FireCall { Result.Nodes.getNodeAs<clang::CallExpr>("fire") };
@@ -59,16 +64,34 @@ public:
 
     auto FireFunctionDecl { llvm::dyn_cast<clang::FunctionDecl>(FireArg->getDecl()) };
     assert(FireFunctionDecl); // XXX
+
+    Rewriter_->ReplaceText(FireCall->getSourceRange(),
+                           "std::printf(\"hello fire\");"); // XXX
   }
+
+private:
+  clang::Rewriter *Rewriter_;
 };
 
 class FireConsumer : public clang::ASTConsumer
 {
 public:
+  FireConsumer(std::string *FileRewritten)
+  : FileRewritten_(FileRewritten)
+  {}
+
   void HandleTranslationUnit(clang::ASTContext &Context) override
   {
     using namespace clang::ast_matchers;
 
+    // create file rewriter
+    auto &SourceManager { Context.getSourceManager() };
+    auto &LangOpts { Context.getLangOpts() };
+
+    clang::Rewriter Rewriter;
+    Rewriter.setSourceMgr(SourceManager, LangOpts);
+
+    // create match expression
     auto FireMatchExpression {
       callExpr(
         callee(
@@ -80,8 +103,10 @@ public:
                   hasName("fire")))))))
     };
 
-    FireMatchCallback MatchCallback;
+    // create match callback
+    FireMatchCallback MatchCallback(&Rewriter);
 
+    // create and run match finder
     clang::ast_matchers::MatchFinder MatchFinder;
     MatchFinder.addMatcher(FireMatchExpression.bind("fire"), &MatchCallback);
 
@@ -96,26 +121,54 @@ public:
 
       Diags.Report(e.where(), ID);
     }
+
+    // copy rewritten file to buffer
+    auto FileID { SourceManager.getMainFileID() };
+    auto FileRewriteBuffer { Rewriter.getRewriteBufferFor(FileID) };
+
+    *FileRewritten_ = std::string(FileRewriteBuffer->begin(),
+                                  FileRewriteBuffer->end());
   }
+
+private:
+  std::string *FileRewritten_;
 };
 
 class FireAction : public clang::PluginASTAction
 {
 protected:
   std::unique_ptr<clang::ASTConsumer> CreateASTConsumer(
-    clang::CompilerInstance &,
+    clang::CompilerInstance &CI,
     llvm::StringRef) override
   {
-    return std::make_unique<FireConsumer>();
+    return std::make_unique<FireConsumer>(&FileRewritten_);
   }
 
   bool ParseArgs(clang::CompilerInstance const &,
                  std::vector<std::string> const &) override
   {
-    return true; // XXX
+    return true;
   }
 
-  // XXX ActionType?
+  clang::PluginASTAction::ActionType getActionType() override
+  {
+    return clang::PluginASTAction::ReplaceAction;
+  }
+
+  bool BeginSourceFileAction (clang::CompilerInstance &)
+  {
+    FileRewritten_.clear();
+
+    return true;
+  }
+
+  void EndSourceFileAction() override
+  {
+    // XXX
+  }
+
+private:
+  std::string FileRewritten_;
 };
 
 } // end namespace
